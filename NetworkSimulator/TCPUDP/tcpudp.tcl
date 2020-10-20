a8b96011c534814b1f9f6691931b88e81ce73a75
Shivanirudh <shivanirudh2001@gmail.com>
2020-10-20 23:44:04 +0530

    A8 Completed

--- NetworkSimulator/TCPUDP/tcpudp.tcl
+++ NetworkSimulator/TCPUDP/tcpudp.tcl
@@ -1,8 +1,13 @@
+#Program to evaluate performance of TCP and UDP connections under bottleneck situation
+
+#1. Create Simulator object
 set ns [new Simulator]
 
+#2. Define different colors for data flows (for NAM)
 $ns color 1 blue
 $ns color 2 red
 
+#6. Create six nodes
 set n0 [$ns node]
 set n1 [$ns node]
 set n2 [$ns node]
@@ -10,11 +15,15 @@ set n3 [$ns node]
 set n4 [$ns node]
 set n5 [$ns node]
 
+#3. Open the Trace files
 set f [open out.tr w]
 $ns trace-all $f
+
+#4. Open the NAM trace file
 set nf [open out.nam w]
 $ns namtrace-all $nf
 
+#7. Create links between the nodes
 $ns duplex-link $n0 $n2 2Mb 10ms DropTail
 $ns duplex-link $n1 $n2 2Mb 10ms DropTail
 $ns simplex-link $n2 $n3 0.3Mb 100ms DropTail
@@ -22,6 +31,7 @@ $ns simplex-link $n3 $n2 0.3Mb 100ms DropTail
 $ns duplex-link $n3 $n4 0.5Mb 40ms DropTail
 $ns duplex-link $n3 $n5 0.5Mb 40ms DropTail
 
+#8. Align it properly
 $ns duplex-link-op $n0 $n2 orient right-up
 $ns duplex-link-op $n1 $n2 orient right-down
 $ns simplex-link-op $n2 $n3 orient right
@@ -29,8 +39,10 @@ $ns simplex-link-op $n3 $n2 orient left
 $ns duplex-link-op $n3 $n4 orient right-up
 $ns duplex-link-op $n3 $n5 orient right-down
 
+#9. Set Queue Size of link (n2-n3) to 10 (or) 5
 $ns queue-limit $n2 $n3 10
 
+#10. Setup a TCP connection over 0 and 4 and its flow id, window size, packet size
 set tcp [new Agent/TCP/Newreno]
 $ns attach-agent $n0 $tcp
 set sink [new Agent/TCPSink/DelAck]
@@ -40,50 +52,41 @@ $tcp set fid_ 1
 $tcp set window_ 8000
 $tcp set packetSize_ 512
 $tcp set class_ 2
+
+#11. Setup a FTP over TCP connection
 set ftp [new Application/FTP]
 $ftp attach-agent $tcp
 
-set udp1 [new Agent/UDP]
-$ns attach-agent $n1 $udp1
-$udp1 set class_ 1
-set cbr1 [new Application/Traffic/CBR]
-$cbr1 attach-agent $udp1
-$cbr1 set type_ CBR
-$cbr1 set packet_size_ 1024
-$cbr1 set rate_ 0.01mb
-$cbr1 set random_ false
+#12. Setup a UDP connection over 1 and 5. Set the flow id
+set udp [new Agent/UDP]
+$ns attach-agent $n1 $udp
+$udp set class_ 1
 
-set udp2 [new Agent/UDP]
-$ns attach-agent $n5 $udp2
-$udp2 set class_ 1
-set cbr2 [new Application/Traffic/CBR]
-$cbr2 attach-agent $udp2
-$cbr2 set type_ CBR
-$cbr2 set packet_size_ 1024
-$cbr2 set rate_ 0.01mb
-$cbr2 set random_ false
+#13. Setup a CBR over UDP connection with type, packet size, rate, random fields
+set cbr [new Application/Traffic/CBR]
+$cbr attach-agent $udp
+$cbr set type_ CBR
+$cbr set packet_size_ 1024
+$cbr set rate_ 0.01mb
+$cbr set random_ false
 
-set null1 [new Agent/Null]
-$ns attach-agent $n1 $null1
+set null [new Agent/Null]
+$ns attach-agent $n5 $null
+$ns connect $udp $null
 
-set null2 [new Agent/Null]
-$ns attach-agent $n5 $null2
+#14. Start and stop the cbr and ftp accordingly
+$ns at 0.1 "$cbr start"
+$ns at 0.5 "$ftp start"
+$ns at 4.7 "$ftp start"
+$ns at 5.0 "$cbr stop"
 
-$ns connect $udp1 $null1
-$ns connect $udp2 $null2
-
-$ns at 0.1 "$cbr1 start"
-$ns at 0.2 "$cbr2 start"
-$ns at 1.0 "$ftp start"
-$ns at 4.5 "$ftp start"
-$ns at 5.0 "$cbr2 stop"
-$ns at 5.1 "$cbr1 stop"
-
+#15. Finish the simulation
 $ns at 7.0 "finish"
 
-puts [$cbr1 set packetSize_]
-puts [$cbr2 set interval_]
+puts "Packet size: [$cbr set packetSize_]"
+puts "Interval: [$cbr set interval_]"
 
+#5. Define a 'finish' procedure
 proc finish {} {
 	global ns f nf
 	$ns flush-trace
